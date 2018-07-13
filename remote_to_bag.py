a783eb62a0a3c753653351134a2871f8eebf2989
mjkrause <michael.j.krause@gmail.com>
2018-07-13 16:27:51 -0700

    dict for list of dict in rfm done, all tests pass

--- remote_to_bag.py
+++ remote_to_bag.py
@@ -6,40 +6,96 @@ import json
 import os
 import requests
 
-"""Creates a BDBag from a remote-file-manifest (see
-    https://github.com/fair-research/bdbag/blob/master/doc/config.md). The
-    input is a data bundle.
+# Code creates a BDBag from data bundles from the DSS. The bag is created
+# froma remote-file-manifest (see
+# https://github.com/fair-research/bdbag/blob/master/doc/config.md). The
+# input is a data bundle.
+#
+# This class has a methods that processes the content of a list of DSS
+# data bundles, breaks it down to the level of data objects, and creates a
+# remote-manifest-file from those data objects. The remote-manifest-file is
+# then used as a configuration file to create a BDBag.
+# derived from a list of bundles, and assembles a remote-file-manifest
+# configuration file that's used to create a bdbag.
 
-    This class has a methods that processes the content of a list of DSS
-    data bundles, breaks it down to the level of data objects, and creates a
-    remote-manifest-file from those data objects. The remote-manifest-file is
-    then used as a configuration file to create a BDBag.
-    derived from a list of bundles, and assembles a remote-file-manifest 
-    configuration file that's used to create a bdbag."""
 
-
-def create_rfm_from_data_objects(base_url, service_url, data_object_id):
-    """Returns a remote-file-manifest from a DSS data objects ID.
-
-    :parameter: (dict) data_object from DOS
-    :returns: JSON file written to local system containing the input"""
-
-    rfm = {}  # dict to hold the manifest
+def create_dict_for_rfm(base_url, service_url, data_object_id, local_fname_id):
+    """Returns a single dictionary of a remote-file-manifest, which is a 
+     list of dictionaries.
+    :parameter: 
+    :returns: (dict)"""
 
     dataobject = DSSDataObject(base_url, service_url, data_object_id)
-    dobj = dataobject.get_object()
-
+    url_ = dataobject.data_object_url
+    length_ = dataobject.get_file_size()
+    filename_ = 'dss_data_object_' + str(local_fname_id)
 
     keys = ['url', 'length', 'filename']
-    vals = [dataobject.data_object_url, ]
+    vals = [url_, length_, filename_]
+    d1 = dict(zip(keys, vals))
+    d2 = dataobject.get_checksums()
 
-    return dataobject.get_object()['id']
+    return dict(d1, **d2)  # concatenate
 
 def make_bag(self, rfm_fname):
     '''data_to_json'''
     rfm_fname = 'remote-file-manifest.json'
     with open(rfm_fname, 'w') as fp:
         json.dump(self.data_obj, fp)
+
+
+class DSSDataObject:
+    def __init__(self, base_url, service_url, data_object_id):
+        self.base_url = base_url
+        self.service_url = service_url
+        self.data_object_id = data_object_id
+        self.data_object_url = os.path.join(self.service_url,
+                                            self.base_url,
+                                            'dataobjects',
+                                            self.data_object_id)
+        self.data_object = requests.get(self.data_object_url).json()['data_object']
+
+    def get_object(self):
+        """
+        :return: 
+        """
+        return self.data_object
+
+    def get_file_size(self):
+        """
+        :returns: file size or length of the data object"""
+
+        # Some data objects have a size attribute, check first...
+        d = self.data_object  # is a dict
+        if 'size' in d.keys():
+            if d['size']:
+                return d['size']
+        elif d['urls']:  # ...while in other objects we check in the first file.
+            if d['urls'][0]['url']:
+                file_url = d['urls'][0]['url']
+                r = requests.head(file_url)
+                if r.status_code == 200:
+                    return r.headers['X-DSS-SIZE']
+                else:
+                    r.raise_for_status()
+
+    def get_checksums(self):
+        L = self.data_object  # list
+        if L['checksums']:
+            L = L['checksums']
+            checksum_types = [d['type'] for d in L]
+            check_sums = [d['checksum'] for d in L]
+            return dict(zip(checksum_types, check_sums))
+
+    def to_disk(self, json_fname):
+        """
+
+        :return: writes data object to disk as JSON file 
+        """
+        data_object_json = json.dumps(self.data_object)
+
+        with open(json_fname, 'w') as fp:
+            json.dump(data_object_json, fp)
 
 
 class DSSBundle:
@@ -99,38 +155,3 @@ class DSSBundle:
         """
         bundle = self.get_data_bundle(bundle_idx)
         return bundle['data_object_ids']
-
-
-class DSSDataObject:
-
-    def __init__(self, base_url, service_url, data_object_id):
-        self.base_url = base_url
-        self.service_url = service_url
-        self.data_object_id = data_object_id
-
-    def __get_data_object_url(self):
-        """
-        :return: (string) URL as instance variable
-        """
-        self.data_object_url = os.path.join(self.service_url,
-                                            self.base_url,
-                                            'dataobjects',
-                                            self.data_object_id)
-
-    def get_object(self):
-        """
-        :return: 
-        """
-        self.__get_data_object_url()
-        data_object = requests.get(self.data_object_url).json()['data_object']
-        return data_object
-
-    def to_disk(self, json_fname):
-        """
-        
-        :return: writes data object to disk as JSON file 
-        """
-        data_object_json = json.dumps(self.get_object())
-
-        with open(json_fname, 'w') as fp:
-            json.dump(data_object_json, fp)
--- test_RemoteToBag.py
+++ test_RemoteToBag.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 import unittest
 import os
 import json
@@ -8,7 +7,7 @@ import requests
 from pprint import pprint
 from remote_to_bag import DSSBundle as Bundle
 from remote_to_bag import DSSDataObject as DataObject
-from remote_to_bag import create_rfm_from_data_objects
+from remote_to_bag import create_dict_for_rfm
 from bdbag import bdbag_api
 
 class Test_RemoteToBag(unittest.TestCase):
@@ -41,6 +40,17 @@ class Test_RemoteToBag(unittest.TestCase):
         self.service_url = \
             "https://ekivlnizh1.execute-api.us-west-2.amazonaws.com/api"
         self.base_url = "ga4gh/dos/v1"
+        self.data_object_id = '8ff23235-4435-4929-8fb2-5d55b4564999'
+        self.aws_sample_url = 'https://commons-dss.ucsc-cgp-dev.org/v1/files/8ff23235-4435-4929-8fb2-5d55b4564999?replica=aws'
+
+        self.create_dict_for_rfm = {
+            'crc32c': '63439d51',
+            'etag': '57db2e71deb4dab5e4b3f251ac9243b0',
+            'filename': 'dss_data_object_5',
+            'length': '5897',
+            'sha1': '05f818a54510272c17dcda69c948f8d904b5aae3',
+            'sha256': 'c873835a74cea9c811cc7799f8897ac480cccf84f631c99b5293900f7a071b53',
+            'url': 'https://ekivlnizh1.execute-api.us-west-2.amazonaws.com/api/ga4gh/dos/v1/dataobjects/8ff23235-4435-4929-8fb2-5d55b4564999'}
 
     def tearDown(self):
         os.remove('rfm.json')
@@ -59,25 +69,44 @@ class Test_RemoteToBag(unittest.TestCase):
                            algs=['sha256'],
                            remote_file_manifest=self.rfm_fname)
 
-    def test_create_rfm_from_data_objects(self):
+    def test_create_dict_for_rfm(self):
+        d = create_dict_for_rfm(self.base_url,
+                                         self.service_url,
+                                         self.data_object_id,
+                                         local_fname_id=5)
 
-        #databundle = Bundle(self.base_url, self.service_url)
-        #pprint(databundle.get_url())
-        #pprint(databundle.get_list())
-        #pprint(databundle.get_data_bundle(0))
-        #pprint(databundle.list_data_object_ids(0))
+        self.assertTrue(len(d), 7)
+        self.assertDictEqual(d, self.create_dict_for_rfm)
 
-        data_object_id = '8ff23235-4435-4929-8fb2-5d55b4564999'
-        dataobject = DataObject(self.base_url, self.service_url, data_object_id)
-        #pprint(dataobject.get_object())
+    def test_return_file_size(self):
 
-        dataobjid = create_rfm_from_data_objects(self.base_url,
-                                                 self.service_url,
-                                                 data_object_id)
-        print(dataobjid)
+        pprint(self.aws_sample_url)
 
-        json_fname = 'test_data_object.json'
-        dataobject.to_disk(json_fname)
+        r = requests.head(self.aws_sample_url)
+        pprint(r.status_code)
+        pprint(r.text)
+        d = r.headers
+        pprint(d)
+
+
+
+class TestDSSDataBundle(unittest.TestCase):
+
+    def setUp(self):
+        # Set URLs to data bundles.
+        self.service_url = \
+            "https://ekivlnizh1.execute-api.us-west-2.amazonaws.com/api"
+        self.base_url = "ga4gh/dos/v1"
+
+    def tearDown(self):
+        pass
+
+    def test_basic(self):
+        databundle = Bundle(self.base_url, self.service_url)
+        pprint(databundle.get_url())
+        pprint(databundle.get_list())
+        pprint(databundle.get_data_bundle(0))
+        pprint(databundle.list_data_object_ids(0))
 
         # # Create remote-to-bag instance.
         # rtb = RemoteToBag(self.base_url, self.service_url)
@@ -90,4 +119,57 @@ class Test_RemoteToBag(unittest.TestCase):
 
 
 
+class TestDSSDataObject(unittest.TestCase):
+
+    def setUp(self):
+
+        # Set URLs to data bundles.
+        self.service_url = \
+            "https://ekivlnizh1.execute-api.us-west-2.amazonaws.com/api"
+        self.base_url = "ga4gh/dos/v1"
+        self.data_object_id1 = '8ff23235-4435-4929-8fb2-5d55b4564999'
+        self.data_object_id2 = '46df9ac5-0e72-4aa5-8e6b-6b55b362c29a'
+
+    def tearDown(self):
+        pass
+
+    def test_get_object(self):
+
+        dataobject = DataObject(self.base_url,
+                                self.service_url,
+                                self.data_object_id1)
+
+        pprint(dataobject.get_object())
+
+        dataobject = DataObject(self.base_url,
+                                self.service_url,
+                                self.data_object_id2)
+        pprint(dataobject.get_object())
+
+    def test_get_file_size(self):
+        # Size from object.
+        dataobject = DataObject(self.base_url,
+                                self.service_url,
+                                self.data_object_id2)
+
+        d = dataobject.get_object()
+        self.assertEqual(dataobject.get_file_size(), d['size'])
+
+        # Size extracted first URL in self.data_object_id1.
+        dataobject = DataObject(self.base_url,
+                                self.service_url,
+                                self.data_object_id1)
+        s3_url = ('https://commons-dss.ucsc-cgp-dev.org/v1/files/'
+                  '8ff23235-4435-4929-8fb2-5d55b4564999?replica=aws')
+        r = requests.head(s3_url)
+        size_from_s3_url = r.headers['X-DSS-SIZE']
+        self.assertEqual(dataobject.get_file_size(), size_from_s3_url)
+
+    def test_get_checksums(self):
+        dataobject = DataObject(self.base_url,
+                                self.service_url,
+                                self.data_object_id1)
+        d = dataobject.get_checksums()
+
+        self.assertEqual(d['sha1'], '05f818a54510272c17dcda69c948f8d904b5aae3')
 
